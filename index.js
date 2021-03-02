d26f6442bdc5a5fcb331e0fb789cd0786010188c
Hoang Vu <hoangvu.26may@gmail.com>
2021-03-01 19:16:49 -0500

    update

--- index.js
+++ index.js
@@ -1,21 +1,56 @@
 const path = require('path')
 const PORT = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
 
 const express = require('express');
 const exphbs  = require('express-handlebars');
 
 const app = express();
 
+const request = require('request');
+
+
+//body parser
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+
+//api
+function apiCall(finishedAPI, symbol){
+    request('https://cloud.iexapis.com/stable/stock/'+symbol+'/quote?token=pk_15789549725946bd8073caab76710b2d', { json: true }, (err, res, body) => {
+	if (err) {return console.log(err);}
+	if (res.statusCode === 200){
+		//console.log(body);
+		finishedAPI(body);        
+		};
+	});
+};
+
+
+
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-    res.render('home', {
-        stuff: "this is stuff..."
+    apiCall(function(doneAPI){
+        res.render('home', {
+            stock: doneAPI
+        });
     });
+       
 });
 //aaa
-
+app.post('/', function (req, res) {
+    apiCall(function(doneAPI){
+        quotedStock = req.body.stockname;
+        res.render('home', {
+            stock: doneAPI,
+        });
+    }, req.body.stockname);
+       
+});
 
 //static folder
 app.use(express.static(path.join(__dirname, 'public')));
